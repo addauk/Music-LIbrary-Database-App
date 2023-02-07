2ffcf256fe7fbf666207ad9d9158f24f15e90644
addauk <addauk@gmail.com>
2023-02-07 15:14:39 +0000

    Get and Post methods implemented for artists and albums

--- spec/integration/application_spec.rb
+++ spec/integration/application_spec.rb
@@ -10,4 +10,56 @@ describe Application do
   # class so our tests work.
   let(:app) { Application.new }
 
+  context 'GET /albums' do
+    it 'shoud return all albums' do
+      response = get('/albums')
+      expected_response = "Surfer Rosa, Waterloo, Super Trouper, Bossanova, Lover, Folklore, I Put a Spell on You, Baltimore, Here Comes the Sun, Fodder on My Wings, Ring Ring"
+      expect(response.status).to eq(200)
+      expect(response.body).to eq(expected_response)
+    
+    end
+  end
+
+  context 'POST /albums' do
+    it 'should create a new album' do
+      response = post(
+        '/albums',
+        title:'OK Computer',
+        release_year: '1997',
+        artist_id: '1'
+      )
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq('')
+
+      response = get('/albums')
+      expect(response.body).to include('OK Computer')
+    end
+  end
+
+  context 'GET /artists' do
+    it 'should return all artists' do
+      response = get('/artists')
+      expected_response = 'Pixies, ABBA, Taylor Swift, Nina Simone, Kiasmos'
+      expect(response.status).to eq(200)
+      expect(response.body).to eq(expected_response)
+    end
+  end
+
+  context 'POST /artists' do
+    it 'creates a new artist' do
+      response = post(
+        '/artists',
+        name:'Radiohead',
+        genre:'Rock'
+      )
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq('')
+
+      response = get('/artists')
+      expect(response.body).to include('Radiohead')     
+    end
+  end
+
 end
